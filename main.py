960142bf91a6ca3a782c17c67c01890b130564ba
shimat <schimatk@gmail.com>
2023-01-16 14:18:43 +0900

    chuoku fix

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import pandas as pd
 import tabula
-
-dfs = tabula.read_pdf(
-    "R5ukeireyotesu1124_sasikae_1125_syuryo.pdf", 
-    lattice=True, 
-    pages="all",
-    pandas_options={"header": [0, 1]}
-)
+import streamlit as st
 
 
+@st.experimental_memo
+def read_pdf():
+    dfs = tabula.read_pdf(
+        "R5ukeireyotesu1124_sasikae_1125_syuryo.pdf",
+        lattice=True,
+        pages="all",
+        pandas_options={"header": [0, 1]}
+    )
+    return dfs
+
+
+dfs = read_pdf()
 for df in dfs:
     ward = df.iloc[:, 0].str.replace("\r", "")[2]
     print(ward)
 
     upper = df.iloc[0:3, 1:]
     lower = df.iloc[3:, :]
-    print(upper)
-    print(lower)	
-    print(upper.shape)
-    print(lower.shape)
-
-    _, upper_cols = upper.shape
-    replace_table = {str(i+1):f"C{i}" for i in range(upper_cols)}
+    replace_table = {i+1: i for i in range(upper.shape[1])}
     upper = upper.rename(columns=replace_table)
-    print(replace_table)
-    print(upper)
-    
-    df = pd.concat([df.iloc[0:2, 1:], df.iloc[2:, :]], axis=0)
-    #print(df)
-    #df.to_excel(f"{ward}.xlsx", index=None)
+
+    df = pd.concat([upper, lower], axis=0)
+    print(df)
+    print(df.iloc[0:2, :])
+
+    columns = df.iloc[0, :]
+    for c in range(6):
+        columns[2 + c*2] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2]
+        columns[2 + c*2+1] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2+1]
+    df.columns = columns
+    df.drop(index=df.index[[0, 1]], columns=df.columns[-1], inplace=True)
+
+    print(df)
+    for c in df.columns[2:]:
+        df[c] = df[c].astype("Int64")
+    df = df.reset_index().drop("index", axis=1)
+
+    st.dataframe(df)
+    # df.to_excel(f"{ward}.xlsx", index=None)
     break
