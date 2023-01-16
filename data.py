baa08dddbbfee9cc2f65c63eaf24a10449fef31c
shimat <schimatk@gmail.com>
2023-01-16 20:39:48 +0900

    all wards

---
+++ data.py
@@ -0,0 +1,50 @@
+import itertools
+import functools
+from typing import Iterable
+import numpy as np
+import pandas as pd
+import tabula
+import streamlit as st
+
+
+@st.experimental_memo
+def read_pdf():
+    dfs = tabula.read_pdf(
+        "R5ukeireyotesu1124_sasikae_1125_syuryo.pdf",
+        lattice=True,
+        pages="all",
+        pandas_options={"header": [0, 1]},
+    )
+    return dfs
+
+
+def combine_dataframes(dfs: Iterable[pd.DataFrame]):
+    def update(df: pd.DataFrame):
+        ward = df.iloc[2, 0].replace("\r", "")
+        if ward == "厚別区":
+            df.iloc[3, 1:15] = df.iloc[3, 0:14]
+            df.drop(index=df.index[[0, 1]], columns=df.columns[0], inplace=True)
+        else:
+            upper = df.iloc[2:3, 1:]
+            lower = df.iloc[3:, :]
+            upper.rename(
+                columns={i + 1: i for i in range(upper.shape[1])}, inplace=True
+            )
+            df = pd.concat([upper, lower], axis=0)
+            df.drop(columns=df.columns[-1], inplace=True)
+
+        df.insert(1, "区", ward)
+        df.columns = ["施設名", "住所", "区"] + list(
+            itertools.chain.from_iterable(
+                (f"{i}歳児受入予定", f"{i}歳児申込") for i in range(0, 6)
+            )
+        )
+        df = df.reset_index().drop("index", axis=1)
+        for c in df.columns[3:]:
+            df[c] = np.floor(pd.to_numeric(df[c], errors="coerce")).astype("Int64")
+        return df
+
+    df_combined = functools.reduce(
+        lambda all, d: pd.concat([all, update(d)]), dfs, None
+    )
+    return df_combined
--- main.py
+++ main.py
@@ -1,45 +1,33 @@
 import pandas as pd
-import tabula
+import numpy as np
 import streamlit as st
+from data import read_pdf, combine_dataframes
 
 
-@st.experimental_memo
-def read_pdf():
-    dfs = tabula.read_pdf(
-        "R5ukeireyotesu1124_sasikae_1125_syuryo.pdf",
-        lattice=True,
-        pages="all",
-        pandas_options={"header": [0, 1]}
-    )
-    return dfs
-
+TITLE = "令和5年札幌市保育園申し込み状況"
+st.set_page_config(page_title=TITLE, layout="wide")
+st.title(TITLE)
+st.markdown("https://kosodate.city.sapporo.jp/mokuteki/azukeru/hoiku/ninka/7656.html")
 
 dfs = read_pdf()
-for df in dfs:
-    ward = df.iloc[:, 0].str.replace("\r", "")[2]
-    print(ward)
+df_combined = combine_dataframes(dfs)
 
-    upper = df.iloc[0:3, 1:]
-    lower = df.iloc[3:, :]
-    replace_table = {i+1: i for i in range(upper.shape[1])}
-    upper = upper.rename(columns=replace_table)
+# df.to_excel(f"{ward}.xlsx", index=None)
 
-    df = pd.concat([upper, lower], axis=0)
-    print(df)
-    print(df.iloc[0:2, :])
 
-    columns = df.iloc[0, :]
-    for c in range(6):
-        columns[2 + c*2] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2]
-        columns[2 + c*2+1] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2+1]
-    df.columns = columns
-    df.drop(index=df.index[[0, 1]], columns=df.columns[-1], inplace=True)
+st.dataframe(df_combined)
 
-    print(df)
-    for c in df.columns[2:]:
-        df[c] = df[c].astype("Int64")
-    df = df.reset_index().drop("index", axis=1)
+# df = df.reindex(columns=column_names)
 
-    st.dataframe(df)
-    # df.to_excel(f"{ward}.xlsx", index=None)
-    break
+# columns = df.iloc[0, :].values
+# for c in range(6):
+#    columns[2 + c*2] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2]
+#    columns[2 + c*2+1] = df.iloc[0, 2+c] + df.iloc[1, 2 + c*2+1]
+# df.columns = columns
+# df.drop(index=df.index[[0, 1]], columns=df.columns[-1], inplace=True)
+# df = df.reindex(columns=["", "", ""])
+
+# for c in df.columns[2:]:
+#    df[c] = df[c].astype("Int64")
+
+# df = df.reset_index().drop("index", axis=1)
